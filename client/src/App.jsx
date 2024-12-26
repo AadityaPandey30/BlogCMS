aebeb932083085405e4b8b4d192191a0118a86ba
Aaditya Pandey <aprock3004@gmail.com>
2024-12-26 21:52:33 +0530

    CRUD running properly

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AppLayout from "./components/AppLayout";
 import BlogList from "./components/BlogList";
 import BlogForm from "./components/BlogForm";
+import Footer from "./components/Footer";
 
 const App = () => {
     return (
@@ -13,6 +14,7 @@ const App = () => {
                     <Route path="blog/edit/:id" element={<BlogForm />} />
                 </Route>
             </Routes>
+            <Footer />
         </Router>
     );
 };
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+
+const Footer = () => {
+  return (
+    <div>
+      <p className="text-[#8f8f8f] text-[12px] fixed bottom-2 right-2">By <Link to="https://sites.google.com/view/aaditya-dev/home">Aaditya</Link></p>
+    </div>
+  )
+}
+
+export default Footer
